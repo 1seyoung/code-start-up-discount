c0806e2e645fd349a127869d9f650cf6377cd1a8
whydev24 <whydev24@gmail.com>
2024-02-06 18:17:14 +0900

    init: spring boot

---
+++ src/main/java/com/whydev/saysno/discount/DiscountApplication.java
@@ -0,0 +1,13 @@
+package com.whydev.saysno.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountApplication.class, args);
+	}
+
+}
